138913da0ed8ade0057c44ff67f634af9b40ad19
--global <kuba.serwin99@gmail.com>
2023-02-07 06:19:16 +0100

    Add MapComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import SidePanelComponent from './components/SidePanelComponent'
 import NavBarComponent from './components/NavBarComponent'
+import MapComponent from './components/MapComponent'
 
 const App = () => (
-  <div className="flex flex-col gap-8">
+  <div className="flex flex-col h-screen">
     <NavBarComponent />
-    <div className="border flex gap-8 w-full h-screen">
+    <div className="flex gap-8 w-full h-full py-8">
       <SidePanelComponent />
-      <div className="bg-teal-100 h-full w-full" />
+      <MapComponent />
     </div>
   </div>
 )
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client'
 import { client } from './api/apolloClient'
 import App from './App'
 import './index.css'
+import 'leaflet/dist/leaflet.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
